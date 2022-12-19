43212292895512f73e518bb0cc96337b845fd913
Jansen Lin <93126390+Jansen-Lin@users.noreply.github.com>
2022-12-20 00:29:32 +0800

    Update

--- scripts/auto_commit.bat
+++ scripts/auto_commit.bat
@@ -4,14 +4,22 @@
 
 @echo off
 
+:: start %SystemRoot%\explorer.exe "C:\Program Files\Git\git-bash.exe"
+
 cd C:\Users\Jansen-Lin\github-local-repo\test_bed_repo\scripts\
 
 set filepath=C:\Users\Jansen-Lin\github-local-repo\test_bed_repo\scripts\blank.txt
 
 echo.Hello >> %filepath%
 
-exit
+cd C:\Users\Jansen-Lin\github-local-repo\test_bed_repo\scripts\
 
 git add C:\Users\Jansen-Lin\github-local-repo\test_bed_repo\scripts\blank.txt
 
 git commit -m "Blank Update"
+
+git push
+
+timeout /t 10
+
+exit()
